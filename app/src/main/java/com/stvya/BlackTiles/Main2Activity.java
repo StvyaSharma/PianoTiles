784ac15e24b99cf1acde89082edb13038d74f10b
StvyaSharma <stvyasharma@gmail.com>
2023-05-20 22:24:06 +1000

    Initial commit

---
+++ app/src/main/java/com/stvya/BlackTiles/Main2Activity.java
@@ -0,0 +1,91 @@
+package com.stvya.BlackTiles;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    public static final String TAG="Main2ctivity";
+    Intent i=null;
+    Button b1,b2,b3;
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main2);
+        Log.d(TAG, "onCreate: has been called");
+        i=new Intent(this,MainActivity.class);
+        final Intent in=getIntent();
+        final String score=in.getStringExtra("SCORE");
+        final String tiles=in.getStringExtra(MainActivity.MAXTILES);
+        b1=(Button)findViewById(R.id.btnnwgme);
+        b2=(Button)findViewById(R.id.btnqt);
+        b3=(Button)findViewById(R.id.btnshare);
+        assert score != null;
+        if(score.equals("FAILED"))
+        {
+            ((TextView)findViewById(R.id.tvs)).setText(score);
+            ((LinearLayout)findViewById(R.id.linear2)).setBackgroundResource(R.color.Red);
+            b1.setBackgroundResource(R.color.Red);
+            b2.setBackgroundResource(R.color.Red);
+            b3.setBackgroundResource(R.color.Red);
+        }
+        else
+        {
+            ((TextView)findViewById(R.id.tvscore)).setText(score + "s");
+        }
+        final Intent i2=new Intent(this,Main4Activity.class);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i2);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(score.equals("FAILED"))
+                {
+                    Toast.makeText(Main2Activity.this, "You have failed", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Intent intent=new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_TEXT,"Hey I just completed "+ tiles +" tiles in "+ score+"seconds!!\nTry my new app and download it...");
+                    intent.putExtra(Intent.EXTRA_SUBJECT,"My Score!!");
+                    startActivity(Intent.createChooser(intent,"Share..."));
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent i2=new Intent(this,Main5Activity.class);
+        i2.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i2.putExtra("EXIT", true);
+        startActivity(i2);
+//            super.onBackPressed();
+    }
+
+}
